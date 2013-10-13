db4750bdaf8555441548e399f9080cda5df539b8
Salvatore Tedde <info@jertix.org>
2013-10-13 22:46:57 +0200

    If Google CDN for jQuery is not available, load local jquery file

--- _include/head.php
+++ _include/head.php
@@ -21,6 +21,7 @@
 	
 	<!-- [JavaScripts] -->    
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>if (!window.jQuery) {document.write('<script src="js/jquery.min.js"><\/script>');}</script>
 	<script src="js/javascript.js"></script>
 	<script src="js/settings.js"></script>
 	<!-- [/End JavaScripts] -->   
