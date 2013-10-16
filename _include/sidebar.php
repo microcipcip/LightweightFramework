4f6d60c572ccdd9c30c471076cfa94070425168f
Salvatore Tedde <info@jertix.org>
2013-10-16 20:50:46 +0200

    Send renamed popup and sidebar files online

---
+++ _include/sidebar.php
@@ -0,0 +1,44 @@
+<?php
+	
+?>
+<!-- [Sidebar] -->
+<aside id="side">
+
+	<div class="side">
+		<div class="side-title">Title here</div>
+		<div class="side-content">
+			<p>Fusce nec sapien tortor, vel accumsan mi. Nufelis. Phasellus pulvinar velit quis lorem lobortis a rhoncus est varius.</p>
+			<p>Fusce nec sapien tortor, vel accumsan mi. Nufelis. Phasellus pulvinar velit quis lorem lobortis a rhoncus est varius.</p>
+			<p>Fusce nec sapien tortor, vel accumsan mi. Nufelis. Phasellus pulvinar velit quis lorem lobortis a rhoncus est varius.</p>
+			<p>Fusce nec sapien tortor, vel accumsan mi. Nufelis. Phasellus pulvinar velit quis lorem lobortis a rhoncus est varius.</p>
+		</div>
+	</div>
+		
+	<div class="side">
+		<div class="side-title">Title here</div>
+		<div class="side-content">
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+				<li>Mauris vulputate dui et ante aliquet eu pretium magna mollis.</li>
+				<li>Mauris varius elementum ante, et bibendum arcu venenatis in.</li>
+				<li>Donec consequat nunc id est aliquam at lobortis dolor tempus.</li>
+				<li>Donec rutrum massa sit amet sem adipiscing dictum euismod et leo.</li>
+				<li>Vivamus varius tristique sem, vel mollis lacus volutpat tempus.</li>
+				<li>Nullam sed odio sed enim ultricies gravida.</li>
+				<li>Morbi condimentum gravida tellus, vel hendrerit dolor varius ac.</li>
+			</ul>
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+				<li>Mauris vulputate dui et ante aliquet eu pretium magna mollis.</li>
+				<li>Mauris varius elementum ante, et bibendum arcu venenatis in.</li>
+				<li>Donec consequat nunc id est aliquam at lobortis dolor tempus.</li>
+				<li>Donec rutrum massa sit amet sem adipiscing dictum euismod et leo.</li>
+				<li>Vivamus varius tristique sem, vel mollis lacus volutpat tempus.</li>
+				<li>Nullam sed odio sed enim ultricies gravida.</li>
+				<li>Morbi condimentum gravida tellus, vel hendrerit dolor varius ac.</li>
+			</ul>				
+		</div>
+	</div>
+	
+</aside>
+<!-- [/End Sidebar] -->	
